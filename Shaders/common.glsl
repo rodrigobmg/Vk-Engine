9b37bc510467a8fc4bf19fe062894b1d31c47895
Stefan <47391841+ostef@users.noreply.github.com>
2025-08-17 11:57:42 +0200

    Misc

--- Shaders/common.glsl
+++ Shaders/common.glsl
@@ -342,11 +342,11 @@ float LinearizeDepth(float d, float z_near, float z_far) {
     return z_near * z_far / (z_far + d * (z_near - z_far));
 }
 
-float3 BlendRGBPostmultipliedAlpha(float3 dst, float3 src, float alpha) {
+float3 BlendRGBPostMultipliedAlpha(float3 dst, float3 src, float alpha) {
     return src * alpha + dst * (1 - alpha);
 }
 
-float3 BlendRGBPremultipliedAlpha(float3 dst, float3 src, float alpha) {
+float3 BlendRGBPreMultipliedAlpha(float3 dst, float3 src, float alpha) {
     return src + dst * (1 - alpha);
 }
 
