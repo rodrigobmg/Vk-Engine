20e9db88034765e120ef61e7f7a84a3b53e8a1e9
Stefan <oumansour.stefan@gmail.com>
2025-08-22 21:10:37 +0200

    Use samplers for blur downsample and upsample

--- Shaders/bloom_downsample.comp.glsl
+++ Shaders/bloom_downsample.comp.glsl
@@ -4,18 +4,19 @@ layout(local_size_x=16, local_size_y=16) in;
 
 DECLARE_PER_FRAME_PARAMS();
 
-layout(set=1, binding=0) readonly uniform image2D u_src_image;
+layout(set=1, binding=0) uniform sampler2D u_src_texture;
 layout(set=1, binding=1) writeonly uniform image2D u_dst_image;
 
 void main() {
-    int2 src_size = imageSize(u_src_image);
     int2 dst_size = imageSize(u_dst_image);
-    int2 src_coords = int2(gl_GlobalInvocationID.xy / float2(dst_size) * src_size);
     int2 dst_coords = int2(gl_GlobalInvocationID.xy);
     if (dst_coords.x >= dst_size.x || dst_coords.y >= dst_size.y) {
         return;
     }
 
-    float4 color = DownsampleBox13(u_src_image, src_coords, src_size);
+    int2 src_size = textureSize(u_src_texture, 0);
+    float2 src_coords = IntegerToNormalizedTexCoords(dst_coords, dst_size);
+
+    float4 color = DownsampleBox13(u_src_texture, src_coords, 1 / float2(src_size));
     imageStore(u_dst_image, dst_coords, color);
 }
--- Shaders/bloom_filter.comp.glsl
+++ Shaders/bloom_filter.comp.glsl
@@ -4,19 +4,20 @@ layout(local_size_x=16, local_size_y=16) in;
 
 DECLARE_PER_FRAME_PARAMS();
 
-layout(set=1, binding=0) readonly uniform image2D u_src_image;
+layout(set=1, binding=0) uniform sampler2D u_src_texture;
 layout(set=1, binding=1) writeonly uniform image2D u_dst_image;
 
 void main() {
-    int2 src_size = imageSize(u_src_image);
     int2 dst_size = imageSize(u_dst_image);
-    int2 src_coords = int2(gl_GlobalInvocationID.xy / float2(dst_size) * src_size);
     int2 dst_coords = int2(gl_GlobalInvocationID.xy);
     if (dst_coords.x >= dst_size.x || dst_coords.y >= dst_size.y) {
         return;
     }
 
-    float3 hdr_color = DownsampleBox13WithKarisAverage(u_src_image, src_coords, src_size).rgb;
+    int2 src_size = textureSize(u_src_texture, 0);
+    float2 src_coords = IntegerToNormalizedTexCoords(dst_coords, dst_size);
+
+    float3 hdr_color = DownsampleBox13WithKarisAverage(u_src_texture, src_coords, 1 / float2(src_size)).rgb;
     hdr_color = max(hdr_color, 0.0001);
 
     float brightness = max(hdr_color.r, max(hdr_color.g, hdr_color.b));
